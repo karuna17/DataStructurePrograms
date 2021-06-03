6a1debe4c093b625468fa589de2fe9b939129df1
karuna17 <karunakakade8@gmail.com>
2021-06-04 01:31:27 +0530

    Added: add elements in linkedlist

--- LinkedListDemo.java
+++ LinkedListDemo.java
@@ -1,14 +1,53 @@
 import java.util.LinkedList;
 
 public class LinkedListDemo {
-	
-	
-	public static void main(String[] args) {
-		LinkedList<Integer> list = new LinkedList<Integer>();
-		list.add(56);
-		list.add(30);
-		list.add(70);
+
+	class Node {
+		int data;
+		Node nextNode;
+
+		public Node(int data) {
+			this.data = data;
+			this.nextNode = null;
+		}
+	}
+
+	public Node head = null;    
+	public Node tail = null;    
+
+	public void addNode(int data) {
+		Node newNode = new Node(data);
+
+		if(head == null) {
+			head = newNode;
+			tail = newNode;
+		}else {
+			tail.nextNode = newNode;
+			tail = newNode;
+		}
+	}
+
+	public void displayData() {
+		Node current = head;
 		
-		System.out.println(list);
+		if(head == null) {
+			System.out.println("List is empty");
+		}
+		System.out.println("Nodes of singly linked list");
+		
+        while(current != null) {
+        	System.out.print(current.data+" ");
+        	current = current.nextNode;
+        }
+        System.out.println();
+	}
+	public static void main(String[] args) {
+		LinkedListDemo list = new LinkedListDemo();
+		list.addNode(56);
+		list.addNode(30);
+		list.addNode(70);
+		
+		list.displayData();
+
 	}
 }
