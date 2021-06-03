8fe296075586697f2e4372a08aa4135ed2d7ba97
karuna17 <karunakakade8@gmail.com>
2021-06-03 22:44:21 +0530

    Added: create simple linkedlist

--- LinkedListDemo.java
+++ LinkedListDemo.java
@@ -1,6 +1,14 @@
+import java.util.LinkedList;
 
 public class LinkedListDemo {
+	
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome To Linked List Program");
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(56);
+		list.add(30);
+		list.add(70);
+		
+		System.out.println(list);
 	}
 }
