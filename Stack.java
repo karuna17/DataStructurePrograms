6a540b6889b5aeda876d220da031688236b23020
karuna17 <karunakakade8@gmail.com>
2021-06-06 21:18:31 +0530

    Added: Stack Operations and Queue Operations

--- Stack.java
+++ Stack.java
@@ -1,16 +1,49 @@
 public class Stack {
 	Node head;
+	Node headPosition = null, tailPosition = null;
+
 	private class Node {	 
 		int data; 
 		Node nextNode; 
+		public Node(int data) {
+			this.data = data;
+		}
 	}
 
 	public Stack() {
 		this.head = null;
 	}
 
+	public void enque(int data) {
+		Node node = new Node(data);
+		System.out.println("\ninserting items: "+data);
+
+		if(headPosition == null) {
+			headPosition = node;
+			tailPosition = node;
+		}else {
+			tailPosition.nextNode = node;
+			tailPosition = node;
+		}
+	}
+
+	public int deque() {
+		if(headPosition == null) {
+			System.out.println("stack is underflow");
+		}
+		Node temp = headPosition;
+		System.out.println("Removing: "+temp.data);
+		headPosition = headPosition.nextNode;
+
+		if(headPosition == null) {
+			tailPosition = null;
+		}
+		int value = temp.data;
+		return value;
+	}
+
 	public void push(int data) {
-		Node newNode = new Node();
+		Node newNode = new Node(data);
 
 		if(newNode == null) {
 			System.out.println("Stack Overflow");
@@ -41,15 +74,14 @@ public class Stack {
 			return -1;
 		}
 	}
-	
-    public void display() {
-        Node temp = head;
-        while (temp != null) {
-            System.out.print("\nElements: "+ temp.data);
-            temp = temp.nextNode;
-        }
 
-    }
+	public void display() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.print("\nElements: "+ temp.data);
+			temp = temp.nextNode;
+		}
+	}
 
 	public static void main(String[] args) {
 		Stack s = new Stack();
@@ -58,12 +90,15 @@ public class Stack {
 		s.push(40);
 		s.push(50);
 		s.display();
-		
+
 		System.out.println("\npeek element: "+ s.peek());
-		
+
 		System.out.println("After pop");
 		s.pop();
 		s.display();
+		
+		s.enque(60);
+		s.deque();
 	}
 
 }
